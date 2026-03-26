4f0f15074f6035948f7140f385a830ae3efb634b
Abhay-Pratap200001 <abhaypratap94310@gmail.com>
2026-03-26 11:33:51 +0530

    Build navbar

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 const page = () => {
   return (
     <div>
-      <h1 className='bg-red-600 pl-[50%]'>BookAi</h1>
+      {/* <h1 className='bg-red-600 pl-[50%]'>BookAi</h1> */}
     </div>
   )
 }
